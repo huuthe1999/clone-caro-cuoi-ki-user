88b9d074c19e73a1d3ac40f3156a1953f6bdc1f7
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2020-12-11 14:50:51 +0700

    first commit

---
+++ build/precache-manifest.713c83ef0ab4bd77e692b925bf8fe1f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09aaacc8af472263b7af6ec44634d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d03ac57e2e540662989",
+    "url": "/static/css/main.b1b93b40.chunk.css"
+  },
+  {
+    "revision": "7759e3f115596181020b",
+    "url": "/static/js/2.33b612f6.chunk.js"
+  },
+  {
+    "revision": "62b434ad4fd9c0cc476a5630cadaca38",
+    "url": "/static/js/2.33b612f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d03ac57e2e540662989",
+    "url": "/static/js/main.ee6fac36.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
